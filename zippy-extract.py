67780255a01712768052946a8ec860b0a8a081cc
Sparoney <aaronp9595@gmail.com>
2021-04-02 18:10:23 +1100

    Add files

---
+++ zippy-extract.py
@@ -0,0 +1,18 @@
+from zippyshare_downloader import Zippyshare
+import time
+
+zippy = Zippyshare()
+
+f_out = open("direct-links.txt", "w+")
+
+with open('links.txt') as f_in: 
+    lines = list(line for line in (l.strip() for l in f_in) if line)
+    for i in lines:
+        info = zippy.extract_info(i, download=False)
+        print(info['download_url'])
+        f_out.write(info['download_url'])
+        f_out.write("\n")
+
+
+f_in.close
+f_out.close
