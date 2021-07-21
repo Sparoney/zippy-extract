79f037ad5a64f5c5228de41cde9e158948739524
103061974 <103061974@student.swin.edu.au>
2021-07-21 17:27:09 +1000

    Update to match API changes

--- zippy-extract.py
+++ zippy-extract.py
@@ -1,17 +1,14 @@
-from zippyshare_downloader import Zippyshare
-
-zippy = Zippyshare()
+from zippyshare_downloader import extract_info
 
 f_out = open("direct-links.txt", "w+")
 
 with open('links.txt') as f_in: 
     lines = list(line for line in (l.strip() for l in f_in) if line)
     for i in lines:
-        info = zippy.extract_info(i, download=False)
-        print(info['download_url'])
-        f_out.write(info['download_url'])
+        file = extract_info(i, download=False)
+        print(file.download_url)
+        f_out.write(file.download_url)
         f_out.write("\n")
-
 
 f_in.close
 f_out.close
